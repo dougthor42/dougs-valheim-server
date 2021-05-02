60420a35ec136dac9c96996d66f1de101b446e8c
Douglas Thor <doug.thor@gmail.com>
2021-05-01 17:43:17 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+INSTALL_REQUIRES = ["boto3==1.17.62", "click==7.1.2"]
+CLASSIFIERS = []
+
+ENTRY_POINTS = {
+    "console_scripts": ["dougs_valheim_server = dougs_valheim_server.cli:main"]
+}
+
+setup(
+    name="dougs-valheim-server",
+    version="0.1.0",
+    description="Utility to start/stop my valheim server on AWS.",
+    url="https://github.com/dougthor42/dougs-valheim-server",
+    author="Douglas Thor",
+    author_email="doug.thor@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=False,
+    entry_points=ENTRY_POINTS,
+    python_requires=">=3.6",
+    install_requires=INSTALL_REQUIRES,
+)
